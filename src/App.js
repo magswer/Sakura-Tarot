2c504a03e496df571ee22a8d10c7e5ed105afd14
Magda <ma.wernik@gmail.com>
2022-12-17 20:02:25 +0100

    button css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './components/Header'
 import Header from './components/Header';
 import Main from './components/Main';
+import Button from './components/Button';
 
 function App() {
   return (
     <>
     <Header />
     <Main />
+    <Button />
     </>
   );
 }
