85b3110793033acab106a0b825736b844c27513f
Magda <ma.wernik@gmail.com>
2023-04-20 10:27:01 +0200

    shuffle fixed

--- src/services/functions.js
+++ src/services/functions.js
@@ -1,9 +1,11 @@
-import { shuffle } from "lodash";
 const GetCards = (state) => {
 
     fetch ('https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/')
     .then (resp => resp.json ())
-    .then (data => state(shuffle(data)))
+    .then (data => state(data.sort(() => Math.random() - 0.5)))
+    .catch((error) => {
+        console.log(error);
+    });
 };
 
 export default GetCards;
